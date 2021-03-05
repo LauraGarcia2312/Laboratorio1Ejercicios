9017edc8b93d9c09b4db476396407581a419bf8f
LauraGarcia2312 <laura.garcia@catolica.edu.sv>
2021-03-04 22:35:00 -0600

    Ejercicio2 añadido

---
+++ LabEjercicio2/LabEjercicio2/Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+    Sub Main(args As String())
+        Dim L As Integer = 0
+        Dim A As Integer = 0
+        Dim U As Integer = 0
+        Console.Write("CUANTOS ELEMENTOS: ")
+
+        Dim VEC(0 To A + 1 - 1) As Integer
+
+        For L = 1 To A
+            Console.Write("POSICIÓN {0} ==>", L)
+            U = Console.ReadLine()
+            VEC(L) = Integer.Parse(U)
+        Next
+
+        For L = 1 To A - 1
+
+        Next
+        For S = L + 1 To A
+            If (VEC(L) < VEC(S)) Then
+
+                A = VEC(S)
+                VEC(S) = VEC(L)
+                VEC(L) = A
+
+            End If
+
+        Next
+        Console.WriteLine("orden DESCENDENTE")
+
+    End Sub
+
+End Module
+
+
